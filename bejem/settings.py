d4b8cdc5311ae393f8a2e5bb598664ce3aeb87c8
Anton Konygin <konygin@dcasus.(none)>
2009-10-22 13:52:53 +0600

    add bejem_db, User model and templates

--- bejem/settings.py
+++ bejem/settings.py
@@ -1,20 +1,23 @@
-# Django settings for bejem project.
+import os.path
+
+# Django settings for bejem project
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
+     ('kalashnikov', 'akalashnikov@dc.ru'),
+     ('konygin', 'konygin@gmail.com'),
 )
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = ''           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = ''             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASE_ENGINE = 'mysql'            # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'bejem_db'           # Or path to database file if using sqlite3.
+DATABASE_USER = 'bejem_admin'        # Not used with sqlite3.
+DATABASE_PASSWORD = 'bejem_password' # Not used with sqlite3.
+DATABASE_HOST = ''                   # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''                   # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -66,14 +69,15 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'bejem.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/'),
 )
 
 INSTALLED_APPS = (
     'django.contrib.auth',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'bejem.users',
 )
